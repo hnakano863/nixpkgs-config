94d40a7422dbca9e2b6e852771463402d1137c30
Hiroshi Nakano <hnakano@tlv.co.jp>
2020-08-14 18:26:32 +0900

    Added python-Levenstein package.

--- python3/default.nix
+++ python3/default.nix
@@ -1,9 +1,12 @@
 { pkgs }:
 pkgs.python3.override {
   packageOverrides = self: super: rec {
-    autograd-gamma = super.callPackage ./autograd-gamma/default.nix { pkgs = super; };
+    autograd-gamma =
+      super.callPackage ./autograd-gamma/default.nix { pkgs = super; };
 
     lifelines = super.callPackage ./lifelines/default.nix { pkgs = super; };
+
+    python-levenstein = super.callPackage ./python-levenstein/default.nix { pkgs = super; };
   };
   self = pkgs.python3;
 }
