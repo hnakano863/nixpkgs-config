32db5ae62ef7c83748ff4c0f0a58ab567397cf4e
hnakano863 <notchi863@gmail.com>
2020-08-15 16:35:21 +0900

    Moved python3 to overlays

--- config.nix
+++ config.nix
@@ -3,8 +3,4 @@
   allowUnfree = true;
   allowUnsupportedSystem = false;
   allowInsecure = false;
-  packageOverrides = pkgs:
-    with pkgs; rec {
-      python3 = import ./python3/default.nix { inherit pkgs; };
-    };
 }
